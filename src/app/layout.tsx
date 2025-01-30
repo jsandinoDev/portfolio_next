693f0874639186f304275449b19be66d80001fa3
Josue Sandino <josue.271168@gmail.com>
2025-01-30 00:52:30 -0600

    feat: 🐥 Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Jersey_15  } from "next/font/google";
+import { Jersey_15 } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "../components/NavBar";
 import { Background } from "@/components/Background";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const jersey15 = Jersey_15({
-    variable: "--font-jersey-15",
-    subsets: ["latin"],
-    weight: "400", // Specify the weight if required
-  });
+  variable: "--font-jersey-15",
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata: Metadata = {
-  title: "JsandinoDev Portfolio",
+  title: "JsandinoDev",
   description: "Fullstack Developer",
+  icons: {
+    icon: "/favicon.ico", 
+  },
 };
 
 export default function RootLayout({
@@ -32,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${jersey15.variable} antialiased`}
-      >
+      <body className={` ${jersey15.variable} antialiased`}>
         <Background />
         <NavBar />
         {children}
